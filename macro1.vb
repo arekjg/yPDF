ee7a72887bff4857d761b3582035d757af25174a
arekjg <arekjg@gmail.com>
2022-04-09 11:13:34 +0200

    added macro1.vb file, just for display

---
+++ macro1.vb
@@ -0,0 +1,127 @@
+Sub yPDF(projectNum As String, filePath As String)
+'
+' yPDF Makro
+'
+
+    'Deleting all existing data from sheet
+    Cells.Select
+    Selection.ClearContents
+
+    'Copying data from csv file
+    With ActiveSheet.QueryTables.Add(Connection:= _
+        "TEXT;" & filePath, Destination:=Range("$A$1"))
+        .FieldNames = True
+        .RowNumbers = False
+        .FillAdjacentFormulas = False
+        .PreserveFormatting = True
+        .RefreshOnFileOpen = False
+        .RefreshStyle = xlInsertDeleteCells
+        .SavePassword = False
+        .SaveData = True
+        .AdjustColumnWidth = True
+        .RefreshPeriod = 0
+        .TextFilePromptOnRefresh = False
+        .TextFilePlatform = 1250
+        .TextFileStartRow = 1
+        .TextFileParseType = xlDelimited
+        .TextFileTextQualifier = xlTextQualifierDoubleQuote
+        .TextFileConsecutiveDelimiter = False
+        .TextFileTabDelimiter = False
+        .TextFileSemicolonDelimiter = True
+        .TextFileCommaDelimiter = False
+        .TextFileSpaceDelimiter = False
+        .TextFileColumnDataTypes = Array(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+        .TextFileTrailingMinusNumbers = True
+        .Refresh BackgroundQuery:=False
+    End With
+
+    'Setting up the variable
+    'Dim projectNum As String
+    
+    'InputBox
+    'projectNum = Application.InputBox("Insert project number", Title:="Project Number", Type:=2)
+    
+    'Additional variables
+    char = projectNum & "-"
+    projPage = projectNum & "/"
+    
+    'Inserting variables to cells
+    Range("AM1001") = projPage
+    Range("AN1001") = char
+       
+    'Formulas - finding columns
+    Range("R1").Select
+    ActiveCell.FormulaR1C1 = _
+        "=IF(OR(" & Chr(10) & "(ISNUMBER(FIND(R1001C39,RC[-17])))," & Chr(10) & "(ISNUMBER(FIND(R1001C40,RC[-17])))" & Chr(10) & ")," & Chr(10) & "1,0)"
+    Range("R1").Select
+    Selection.AutoFill Destination:=Range("R1:AF1"), Type:=xlFillDefault
+    Range("R1:AF1").Select
+    Range("AH1").Select
+    ActiveCell.FormulaR1C1 = _
+        "=IF(OR(RC[-16]=1,RC[-15]=1,RC[-14]=1,RC[-13]=1,RC[-12]=1,RC[-11]=1,RC[-10]=1,RC[-9]=1,RC[-8]=1,RC[-7]=1,RC[-6]=1),1,""x"")"
+    
+    'Fomulas - counting columns
+    Range("AI1").Select
+    ActiveCell.FormulaR1C1 = _
+        "=IF(RC[-17]=1,1," & Chr(10) & "IF(RC[-16]=1,2," & Chr(10) & "IF(RC[-15]=1,3," & Chr(10) & "IF(RC[-14]=1,4," & Chr(10) & "IF(RC[-13]=1,5," & Chr(10) & "IF(RC[-12]=1,6," & Chr(10) & "IF(RC[-11]=1,7," & Chr(10) & "IF(RC[-10]=1,8," & Chr(10) & "IF(RC[-9]=1,9," & Chr(10) & "IF(RC[-8]=1,10," & Chr(10) & "IF(RC[-7]=1,11," & Chr(10) & "IF(RC[-6]=1,12," & Chr(10) & "IF(RC[-5]=1,13," & Chr(10) & "IF(RC[-4]=1,14," & Chr(10) & "IF(RC[-3]=1,15," & Chr(10) & "0)))))))))))))))"
+    Range("AJ1").Select
+    ActiveCell.FormulaR1C1 = _
+        "=16-(IF(RC[-4]=1,1," & Chr(10) & "IF(RC[-5]=1,2," & Chr(10) & "IF(RC[-6]=1,3," & Chr(10) & "IF(RC[-7]=1,4," & Chr(10) & "IF(RC[-8]=1,5," & Chr(10) & "IF(RC[-9]=1,6," & Chr(10) & "IF(RC[-10]=1,7," & Chr(10) & "IF(RC[-11]=1,8," & Chr(10) & "IF(RC[-12]=1,9," & Chr(10) & "IF(RC[-13]=1,10," & Chr(10) & "IF(RC[-14]=1,11," & Chr(10) & "IF(RC[-15]=1,12," & Chr(10) & "IF(RC[-16]=1,13," & Chr(10) & "IF(RC[-17]=1,14," & Chr(10) & "IF(RC[-18]=1,15," & Chr(10) & "0))))))))))))))))"
+    Range("AI1:AJ1").Select
+    Selection.AutoFill Destination:=Range("AI1:AJ1000"), Type:=xlFillDefault
+    Range("AI1:AJ1000").Select
+    Range("AI1").Select
+        
+    'Deleting rows (with bool=0)
+    Range("R1:AH1").Select
+    Selection.AutoFill Destination:=Range("R1:AH1000"), Type:=xlFillDefault
+    Range("R1:AH1000").Select
+    Columns("AH:AH").Select
+    Selection.SpecialCells(xlCellTypeFormulas, 2).Select
+    Selection.EntireRow.Delete
+    Range("AH1").Select
+    
+    'Copying important columns
+    colStart = Range("AI1").Value
+    colEnd = Range("AJ1").Value
+    Range(Cells(1, colStart), Cells(1, colEnd)).EntireColumn.Select
+    Selection.Copy
+    Range("AK1").Select
+    ActiveSheet.Paste
+    Columns("A:AJ").Select
+    Range("AJ1").Activate
+    Application.CutCopyMode = False
+    Selection.Delete Shift:=xlToLeft
+    Range("A1").Select
+    
+    'Managing page numbers
+    pCount = Application.WorksheetFunction.CountA(Range("A1").EntireColumn)
+    Range("C1").Select
+    ActiveCell.FormulaR1C1 = _
+        "=SUBSTITUTE(" & Chr(10) & "SUBSTITUTE(" & Chr(10) & "SUBSTITUTE(" & Chr(10) & "SUBSTITUTE(RC[-2],R" & pCount + 1 & "C3,""""),""/00"",""""),""/0"",""""),"" "","""")"
+    Range("C1").Select
+    Selection.AutoFill Destination:=Range("C1:C" & pCount), Type:=xlFillDefault
+    Range("C1:C" & pCount).Select
+    Columns("D:D").Select
+    Selection.Delete Shift:=xlToLeft
+
+    'Copying page numbers to column A
+    Range("C1:C" & pCount).Select
+    Selection.Copy
+    Range("A1").Select
+    Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
+        :=False, Transpose:=False
+        
+    'Converting to numbers
+    Range("A1:A" & pCount).Select
+    With Selection
+        .NumberFormat = "General"
+        .Value = .Value
+    End With
+    
+    'Deleting useless columns
+    Columns("C:C").Select
+    Selection.Delete Shift:=xlToLeft
+    
+    
+End Sub
